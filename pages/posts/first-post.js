8761f89714e6faa2f8ba85a56b15796580b1b046
yandaki <boayves@gmail.com>
2023-09-20 12:55:24 +0000

    Intro to routing pages

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const FirstPost = () => {
+  return (
+    <> 
+        <h1>First Post</h1>
+        <h2>
+            <Link href="/">Back to home !</Link>
+        </h2>
+    </>
+  )
+}
+
+export default FirstPost
