4f680782e43f09544d23f90d2ea96532bba92c72
yandaki <boayves@gmail.com>
2023-09-20 15:17:47 +0000

    implementation styles with modules styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,26 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Script from 'next/script'
 import React from 'react'
+import Layout from '../../components/layout'
 
 const FirstPost = () => {
   return (
-    <> 
+    <Layout> 
+        <Head>
+            <title>First Post</title>
+        </Head>
+        <Script
+            src='https://connect.facebook.net/en_US/sdk.js'
+            strategy='lazyOnload'
+            onLoad={() => console.log("....")}
+
+        />
         <h1>First Post</h1>
         <h2>
             <Link href="/">Back to home !</Link>
         </h2>
-    </>
+    </Layout>
   )
 }
 
